1d395e0a9e8ff7e9aa4538db4f7b9ef9138f29c9
Anna <110973682+AnnaOliinyk666@users.noreply.github.com>
2022-12-17 11:40:31 +0100

    dont understand nothing

--- script.js
+++ script.js
@@ -1,48 +1,38 @@
-function power(x, n) {
-    if (n == 0) {
-        return 1;
-    }
 
-    let result = x;
+    let count = 0;
+    let arr = [2,6,87,1,0,52,42,1,5,7];
+    quickSort(arr,0,arr.length -1);
 
-    for (let i = 1; i < n; i++) {
-        result *= x;
-    }
 
-    return result;
+
+function sort (arr) {
+   quickSort(arr,0,arr.lenght -1);
 }
 
-function powerRec(x, n) {
-    if (n == 0) {
-        return 1;
+function quickSort(arr, countLow, countHigh) {
+    if (countLow < countHigh) {
+        let pivotIndex = findPivotIndex(arr, countLow, countHigh);
+        quickSort(arr, countLow, pivotIndex -1);
+        quickSort(arr, pivotIndex + 1, countHigh);
     }
-
-    return x * powerRec(x, n - 1);
 }
 
-function powerRecImproved(x, n) {
-    if (n == 0) {
-        return 1;
+function findPivotIndex(arr, countLow, countHigh) {
+    let pivot = arr[countHigh];
+    let pivotIndex = countLow - 1;
+    for (let i = countLow; i < countHigh; i++) {
+        count++;
+        if (arr[i] <= pivot) {
+            pivotIndex++;
+            let tmp = arr[pivotIndex];
+            arr[pivotIndex] = arr[i];
+            arr[i] = tmp;
+        }
+        
     }
+    let tmp = arr[pivotIndex + 1];
+    arr[pivotIndex + 1] = arr[countHigh];
+    arr[countHigh] = tmp;
 
-    let result = powerRecImproved(x, Math.floor(n/2));
-
-    if (n%2===0) {
-        result*=result;
-    } else {
-        result = result*result*x;
-    }
-
-    return result;
-    // 1. Получить значение, равное x в степени n / 2
-    // 2. Округлить полученное значение вниз до целого
-    // 3. Выполнить проверку n на четность
-    // 3.1. Если n четное, возвести полученное число в квадрат
-    // 3.2. В противном случае возвести в квадрат и умножить на x
+    return pivotIndex + 1;
 }
- console.log(powerRecImproved(3, 5));
- console.log(power(3, 5));
-
- ////////////////////////////////////////////////////////
-
- 
